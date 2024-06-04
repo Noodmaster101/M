44df8b41fad1bbb99cbe6508bfe87c1230199abe
Noodmaster101 <flukepreech@gmail.com>
2024-06-04 14:40:43 +0700

    Add files via upload

---
+++ src/app/service/page.js
@@ -0,0 +1,18 @@
+
+import Link from "next/link"
+export default function Home() {
+  return (
+    <> 
+    <center>
+      <h1>service </h1>
+    </center>
+
+    <center> <h1> 
+      <Link href="/"> หน้าแรก</Link> |
+      <Link href="/about"> เกี่ยวกับเรา</Link> |
+      <Link href="/service"> บริการของเรา</Link> |      
+      <Link href="/contact"> ติดต่อเรา</Link> |
+        </h1></center>
+    </>
+  );
+}
